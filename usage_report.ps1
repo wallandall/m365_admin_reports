57e628c6ead3008363811d078d421190f8c9a004
wallandall <wallandall@gmail.com>
2023-01-16 14:47:41 +0100

    Groups

--- usage_report.ps1
+++ usage_report.ps1
@@ -176,37 +176,219 @@ function Get-AssignedPlans{
     )
     $reportname = "\assignedPlans"
     $Path = $CSVPath + $reportname + ".csv"
- 
-    $props = @(
-      'AssignedLicenses', 'UserPrincipalName'
-   )
+    $props = @('AssignedLicenses', 'UserPrincipalName' )
 
-   $mgUsers = Get-MgUser -Filter 'assignedLicenses/$count ne 0' -ConsistencyLevel eventual -CountVariable licensedUserCount -All -Property $props | Select-Object $props
-
-   # Get the SKUs
-   $skus = Get-MgSubscribedSku
-
-   # Build a hashtable for faster lookups
-   $skuHt = @{}
-   foreach ($sku in $skus) {
+    $mgUsers = Get-MgUser -Filter 'assignedLicenses/$count ne 0' -ConsistencyLevel eventual -CountVariable licensedUserCount -All -Property $props | Select-Object $props
+    #$mgUsers
+    $skus = Get-MgSubscribedSku
+    $skuHt = @{}
+    foreach ($sku in $skus) {
      $skuHt[$sku.SkuId] = $sku
-   }
-
-   $userOutput = foreach ($user in $mgUsers) {
-
-     # Resolve the ID to license name
-     $licenses = foreach($license in $user.AssignedLicenses) {
-          $skuHt[$license.SkuId].SkuPartNumber
     }
 
+    $userOutput = foreach ($user in $mgUsers) {    
+        $licenses = foreach($license in $user.AssignedLicenses) {
+        $skuHt[$license.SkuId].SkuPartNumber
+    }
     $user | Add-Member -MemberType NoteProperty -Name Licenses -Value ($licenses -join ',')
     $user | Select-Object -Property 'Licenses', 'UserPrincipalName'
-    
    }
-  
    $userOutput | Export-Csv -Path $Path -NoTypeInformation
-
 } 
+
+function Get-LicensingGroups{
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory = $true)][string]$CSVPath
+    )
+    $reportname = "\LicensingGroups"
+    $Path = $CSVPath + $reportname + ".csv"
+    
+  
+    $props = @('id', 'DisplayName', 'licenseAssignmentStates', 'assignedLicenses' )
+    $groups = Get-MgGroup -All -Property $props
+
+    $skusHash = @{} # An empty hashtable
+    ##Get all available SKUs
+    Get-MgSubscribedSku | ForEach-Object {
+        $DisplayName = $_.SkuPartNumber 
+       
+        $_ | Add-Member -MemberType NoteProperty -Name DisplayName -Value $DisplayName
+        $skusHash[$_.SkuId] = $_
+    } 
+
+ 
+
+    foreach($group in  $groups){
+        $groupId = $group.id
+        $groupName = $group.DisplayName
+        $hasLicenses = $group.assignedLicenses
+    
+        $licenseString = ""
+        if($hasLicenses){                     
+            foreach($license in $hasLicenses ){            
+                $licenseString +=  $skusHash[$license.SkuId].DisplayName +";"
+            }
+          
+            $members = Get-MgGroupMember -GroupId $groupId -All
+           
+            $owners = Get-MgGroupOwner -GroupId $groupId -All
+
+            $allOwners = foreach($owner in $owners){
+               $o = get-mguser -UserId $owner.Id
+               [PSCustomObject]@{
+                    GroupLicense = $LicenseString
+                    GroupName = $groupName
+                    GroupId = $groupId
+                    ExtensionData = ""
+                    CommonName = $o.givenName
+                    DisplayName = $o.displayName
+                    EmailAddress = $o.UserPrincipalName
+                    GroupMemberType = "Owner"
+                    isLicensed = "TRUE"
+                    LastDirSyncTime = ""
+                    ObjectId = $o.Id
+                    OverallProvisioningStatus = ""
+                    ValidationStatus = ""
+               }
+            }
+
+            $allOwners | Select-Object -Property * | Export-Csv $Path -Append -NoTypeInformation
+            
+            $allUsers =  foreach($member in $members){
+               $user =get-mguser -UserId $member.Id
+               [PSCustomObject]@{
+                    GroupLicense = $LicenseString
+                    GroupName = $groupName
+                    GroupId = $groupId
+                    ExtensionData = ""
+                    CommonName = $user.givenName
+                    DisplayName = $user.displayName
+                    EmailAddress = $user.UserPrincipalName
+                    GroupMemberType = "Member"
+                    isLicensed = "TRUE"
+                    LastDirSyncTime = ""
+                    ObjectId = $member.Id
+                    OverallProvisioningStatus = ""
+                    ValidationStatus = ""
+                }
+            }
+
+            $allUsers | Select-Object -Property * | Export-Csv $Path -Append -NoTypeInformation
+
+        }        
+    }   
+} 
+
+Function Get-LicenseAssignmentPath{
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory = $true)][string]$CSVPath
+    )
+    $reportname = "\LicenseAssignmentPath"
+    $Path = $CSVPath + $reportname + ".csv"    
+    $props = @('LicenseAssignmentStates','id', 'UserPrincipalName' )
+    $users = Get-MgUser -All -Property $props
+    $groupGUIDs = $users.LicenseAssignmentStates.assignedByGroup | Select-Object -unique    
+    $assignmentGroup = ($groupGUIDs | ForEach-Object {Get-MgGroup -GroupId $_}).DisplayName 
+   
+    $skusHash = @{} # An empty hashtable
+    ##Get all available SKUs
+    Get-MgSubscribedSku | ForEach-Object {
+        $DisplayName = $_.SkuPartNumber 
+        $_ | Add-Member -MemberType NoteProperty -Name DisplayName -Value $DisplayName
+        $skusHash[$_.SkuId] = $_
+    }
+    $report = foreach ($user in $users) {
+        if ($user.LicenseAssignmentStates) { 
+            foreach ($assignment in $user.LicenseAssignmentStates) {
+                $assignedLicenseName = $skusHash[$assignment.SkuId].DisplayName                
+                if (-Not $assignedLicenseName) { continue } # This is a zombie license that is not showing in Azure AD purchased SKUs.
+     
+                if ($assignment.AssignedByGroup) {
+                    $assignedDirectly = $False
+                    $assignmentGroup = $True
+                }
+                else {
+                    # Direct License Assignment
+                    $assignedDirectly = $True
+                    $assignmentGroup = $False
+                }
+                [PSCustomObject]@{
+                    ObjectId              = $user.Id
+                    UserPrincipalName  = $user.UserPrincipalName
+                    AssignedDirectly   = $assignedDirectly
+                    AssignedFromGroup  = $assignmentGroup
+                    SKU                = $assignedLicenseName                 
+                }   
+            }
+        }
+        else {
+            # user does not have any assigned license
+            [PSCustomObject]@{
+                ObjectId              = $user.Id
+                UserPrincipalName  = $user.UserPrincipalName
+                AssignedDirectly   = $null 
+                AssignedFromGroup  = $null
+                SKU                = $null                                  
+            }
+        }
+    }
+    $report | Export-Csv $path -Append -NoTypeInformation
+}
+
+Function Get-AdminReport{
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory = $true)][string]$CSVPath
+    )
+    $reportname = "\AdminReport"
+    $path = $CSVPath + $reportname + ".csv"
+    $allAdminRole = Get-MgDirectoryRole 
+    $props = @('LicenseAssignmentStates','DisplayName', 'UserPrincipalName' )
+    
+    $skusHash = @{} # An empty hashtable
+    ##Get all available SKUs
+    Get-MgSubscribedSku | ForEach-Object {
+        $DisplayName = $_.SkuPartNumber 
+        $_ | Add-Member -MemberType NoteProperty -Name DisplayName -Value $DisplayName
+        $skusHash[$_.SkuId] = $_
+    }
+
+    $report = foreach($role in $allAdminRole){
+        $roleID = $Role.Id
+        $roleDisplayName = $role.DisplayName       
+        $admins = Get-MgDirectoryRoleMember  -DirectoryRoleId $roleID 
+        foreach ($admin in $admins){
+            
+            $user = Get-MgUser -UserId $admin.id -Property $props
+            $allLicenses = Get-MgUserLicenseDetail -UserId $admin.id -Property SkuPartNumber, ServicePlans
+        
+            $assignedLicenseName = @()
+            $allLicenses | ForEach-Object {
+                $assignedLicenseName += $_.SkuPartNumber+";"                
+            } 
+
+            if($assignedLicenseName.Length -gt 0){
+                $isLicensed = $true
+            }
+            else{
+                $isLicensed = $False
+            }
+           
+            [PSCustomObject]@{
+                Adminrole = $roleDisplayName
+                Licenses = $assignedLicenseName
+                DisplayName = $user.DisplayName
+                IsLicensed = $isLicensed                
+                UPN = $user.UserPrincipalName
+            } 
+        }
+    }
+    $report | Select-Object -Property * | Export-Csv -notypeinformation -Path $Path 
+} 
+
+
 
 ###### End Functions  #############################
 
@@ -248,34 +430,36 @@ if ($graph_version) {
     
  
     # Get Unlicensed users and save to the output path
-    ##Write-Log -Message "Generating  Unused Licenses ..."
-    ##Get-UnusedLicenseReport -CSVPath $OutPutPath -OrgName $OrgName
+    Write-Log -Message "Generating  Unused Licenses ..."
+    Get-UnusedLicenseReport -CSVPath $OutPutPath -OrgName $OrgName
     
     #Generate Graph Reports
-    ##Write-Log -Message "Generating reports"
-    ##Get-GraphReports -CSVPath $OutPutPath -ReportPeriod "D180"
+    Write-Log -Message "Generating reports"
+    Get-GraphReports -CSVPath $OutPutPath -ReportPeriod "D180"
 
     #Generate Audit reports
-    ##Write-Log -Message "Generating audit reports"
-   ## Get-LoginLogs -CSVPath $OutPutPath
+    Write-Log -Message "Generating audit reports"
+    Get-LoginLogs -CSVPath $OutPutPath
 
     #Export user list
-    ##Write-Log -Message "Generating Azure AD Users"
-    ##Get-All-Users -CSVPath $OutPutPath
+    Write-Log -Message "Generating Azure AD Users"
+    Get-All-Users -CSVPath $OutPutPath
 
     #Generate assigned plans
     Write-Log -Message "Generating assigned plans"
     Get-AssignedPlans -CSVPath $OutPutPath
-    #6 Get-LicensingGroups -CSVPath $OutPutPath
-    #7 Get-LicenseAssignmentPath -CSVPath $OutPutPath
-    #8 Get-AdminReport -CSVPath $OutPutPath
 
-  
-
+    #Get licensing groups
+    Write-Log -Message "Getting Licensing groups"    
+    Get-LicensingGroups -CSVPath $OutPutPath
 
 
+    Write-Log -Message "Getting License Assignment Path" 
+    Get-LicenseAssignmentPath -CSVPath $OutPutPath
 
-    
+
+    Write-Log -Message "Getting Admin Report" 
+    Get-AdminReport -CSVPath $OutPutPath
 
     Write-Log -Message "Disconnecting..."
     Disconnect-MgGraph | out-null
